50650976ca24c5f07a4fe97cde877b79095ac15a
Tess van der Wal <tess.wal@student.graafschapcollege.nl>
2025-05-14 10:04:24 +0200

    deel verwijderd voor latere merge

--- public/kostenberekenen.php
+++ public/kostenberekenen.php
@@ -3,23 +3,9 @@
     Voorrijkosten: <input type="text" name="voorrijKosten" id=""><br>
     Aantal uur gewerkt: <input type="text" name="uren" id=""><br>
     Uurtarief: <input type="text" name="uurTarief" id=""><br>
-    <input type="submit" value="berekenen" name="berekenen">
 </form>
 
 <?php
-if (isset($_POST["berekenen"]))
-{
-    $voorrijKosten = $_POST["voorrijKosten"];
-    $uren = $_POST["uren"];
-    $uurTarief = $_POST["uurTarief"];
-    echo $voorrijKosten;
-    echo $uren;
-    echo $uurTarief;
-
-    $totaal = (int)$uren * (int)$uurTarief;
-    echo "<br> $totaal";
-}
-
 
 include_once '../src/kosten.php';
 $kosten = new Kosten();
