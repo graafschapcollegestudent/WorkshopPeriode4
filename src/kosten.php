5a9a1a7c3ead075641f545d7142c0a67e34b7520
Jelle Christenhusz <jelle.christenhusz@student.graafschapcollege.nl>
2025-05-14 10:01:55 +0200

    Mooi

--- src/kosten.php
+++ src/kosten.php
@@ -1,14 +1,16 @@
 <?php
 include_once 'database.php';
 
-class Kosten extends Database {
+class Kosten extends Database
+{
 
-  public function VoegUrenToe($uren){
-    if ($uren == "") {
+  public function VoegUrenToe($uren, $totaalBedrag, $klantId)
+  {
+    if ($uren == "" || $totaalBedrag == "" || $klantId == "") {
       return false;
     } else {
-      $query = "INSERT INTO klusdetails (urenGewerkt) VALUES (?);";
-      $params = [$uren];
+      $query = "INSERT INTO klusdetails (urenGewerkt, totaalBedrag, klantId) VALUES (?, ?, ?);";
+      $params = [$uren, $totaalBedrag, $klantId];
 
       return parent::voerQueryUit($query, $params) > 0;
     }
