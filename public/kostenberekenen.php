59b7bb21e72fd669e800a4410b8d1f4828c1f849
Jelle Christenhusz <jelle.christenhusz@student.graafschapcollege.nl>
2025-05-14 09:31:33 +0200

    Beter

--- public/kostenberekenen.php
+++ public/kostenberekenen.php
@@ -16,26 +16,6 @@ if (isset($_POST["berekenen"]))
     echo $uren;
     echo $uurTarief;
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="" method="post">
-        <input type="text" name="uren" id="uren" placeholder="Uren" required>
-        <br>
-
-        <input type="submit" value="Toevoegen" name="submit">
-    </form>
-</body>
-
-</html>
 
 <?php
 include_once '../src/kosten.php';
